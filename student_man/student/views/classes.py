063c59ec2a72cdc295f2dee6ddae0e6e4597a8f9
hamdi <javs_shao@163.com>
2018-11-18 12:32:33 +0800

    班级_添加班级

--- student_man/student/views/classes.py
+++ student_man/student/views/classes.py
@@ -12,3 +12,16 @@ def get_classer(request):
     cls_list = models.Classes.objects.all()
     return render(request, 'get_classes.html',{'cls_list':cls_list})
 
+def add_classed(request):
+    '''
+    添加班级
+    :param request:
+    :return:
+    '''
+    if request.method == 'GET':
+        return render(request, 'add_classes.html')
+    elif request.method == 'POST':
+        title = request.POST.get('title','')
+        models.Classes.objects.create(title=title)
+        return redirect('/classes.html')
+
