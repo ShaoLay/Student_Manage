f1d0c6b3bd56e5d0b15c9ee648df42d65f029631
hamdi <javs_shao@163.com>
2018-11-18 12:00:01 +0800

    模型类_班级

--- student_man/student/models.py
+++ student_man/student/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Classes(models.Model):
+    '''
+    班级表
+    '''
+    title = models.CharField(max_length=32)
+    a = models.ManyToManyField('Teachers')
+
