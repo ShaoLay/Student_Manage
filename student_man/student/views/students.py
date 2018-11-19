ae01592994a0001de629734f6f6356b316b6c30f
hamdi <javs_shao@163.com>
2018-11-19 12:16:48 +0800

    学生_编辑学生

--- student_man/student/views/students.py
+++ student_man/student/views/students.py
@@ -43,3 +43,27 @@ def del_students(request):
     nid = request.GET.get('nid', '')
     models.Students.objects.filter(id=nid).delete()
     return redirect('/students.html')
+
+def edit_students(request):
+    '''
+    编辑学生
+    :param request:
+    :return:
+    '''
+    if request.method=="GET":
+        nid = request.GET.get('nid', '')
+        obj=models.Students.objects.get(id=nid)
+        cs_list = models.Classes.objects.all()
+        return render(request,'edit_students.html',{'obj':obj,'cs_list':cs_list})
+    elif request.method=="POST":
+        nid=request.POST.get('nid','')
+        u = request.POST.get('username', '')
+        a = request.POST.get('age', '')
+        g = request.POST.get('gender', '')
+        c = request.POST.get('cs', '')
+        models.Students.objects.filter(id=nid).update(
+        username=u,
+        age=a,
+        gender=g,
+        cs_id=c)
+        return redirect('/students.html')
